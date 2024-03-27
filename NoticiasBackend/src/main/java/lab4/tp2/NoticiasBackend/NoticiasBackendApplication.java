979a41c79b06159a8e587580a0d6ec15637aa018
Carolina Solsona <103146207+caro890@users.noreply.github.com>
2024-03-27 11:51:11 -0300

    Add files via upload

---
+++ NoticiasBackend/src/main/java/lab4/tp2/NoticiasBackend/NoticiasBackendApplication.java
@@ -0,0 +1,13 @@
+package lab4.tp2.NoticiasBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticiasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticiasBackendApplication.class, args);
+	}
+
+}
---
+++ NoticiasBackend/src/test/java/lab4/tp2/NoticiasBackend/NoticiasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lab4.tp2.NoticiasBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticiasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
